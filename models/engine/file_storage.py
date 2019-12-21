3f3fde8f39a3ceb51250d43228faee44f30ccb5d
Sasha <alex.mapatelian@gmail.com>
2019-12-21 05:27:53 +0000

    fixed code, deleted extra lines

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """This is the file storage class for AirBnB"""
+
 import json
 from models.base_model import BaseModel
 from models.user import User
@@ -17,6 +18,7 @@ class FileStorage:
         __file_path: path to the JSON file
         __objects: objects will be stored
     """
+
     __file_path = "file.json"
     __objects = {}
 
