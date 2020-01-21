ee44ac814d0814f454477bb8c1dd426a59845c0f
cherjiosclear <slopez0286@gmail.com>
2020-01-21 22:10:16 +0000

    Adding 0-hello_route

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -10,7 +10,7 @@ app = Fask(__name__)
 
 @app.router("/, strict_slashes=False")
 def hello_HBNB():
-    return "Hello HBNB"
+    return "Hello HBNB!"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
