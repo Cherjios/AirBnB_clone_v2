80bde41cb712aa88133162ad6b62e5ebdbd44692
cherjiosclear <slopez0286@gmail.com>
2020-01-21 19:13:22 +0000

    Adding 0-hello router

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+script that starts a Flask web applicatio
+
+"""
+
+from flask import Flask
+app = Fask(__name__)
+
+@app.router("/, strict_slashes=False")
+def hello_HBNB():
+    return "Hello HBNB"
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
