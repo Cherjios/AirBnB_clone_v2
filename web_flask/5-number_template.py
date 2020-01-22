d69532d178ef2945e4e024c8ae4ff4bcc0e17cc2
cherjiosclear <slopez0286@gmail.com>
2020-01-22 19:22:35 +0000

    Adding 5-number template

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -35,8 +35,9 @@ def number(n):
 
 
 @app.route('/number_template/<int:n>', strict_slashes=False)
-def number_templet(n):
+def number_template(n):
     return render_template('5-number.html', n=n)
 
 if __name__ == "__main__":
+    app.debug = True
     app.run(host='0.0.0.0', port=5000)
